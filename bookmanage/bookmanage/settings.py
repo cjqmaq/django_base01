bf2a75b35e08a53fc3d40bad85b2bfa65c0518a3
maaoqiang <1025301061@qq.com>
2022-01-20 12:48:11 +0800

    '定义模型'

--- bookmanage/bookmanage/settings.py
+++ bookmanage/bookmanage/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# 注册，安装子应用
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'book',  #方案1
+    'book.apps.BookConfig'  #方案2
 ]
 
 MIDDLEWARE = [
